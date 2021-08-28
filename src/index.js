317573bd018c31198ce6104dc570e1e6bd6ea360
Maxim Sudakov <grax922@gmail.com>
2021-08-28 21:06:41 +0300

    feat: implement func expressionCalculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  let arr = [...expr];
+  let index = arr.indexOf('0') -1;
+  if( arr[index -1] === '/') {
+    throw "TypeError: Division by zero.";
+  }
+  try {
+    const result = (new Function(`return` + ' ' + expr + ' '))(); 
+    if(result === Infinity) { 
+      throw "TypeError: Division by zero.";
+    }else if (typeof(result) === 'number') {
+      return result;
+    }
+  }catch (expr) {
+    if (expr instanceof SyntaxError) {
+      throw "ExpressionError: Brackets must be paired";
+    } 
+  }
+  throw "TypeError: Division by zero.";
 }
 
 module.exports = {
